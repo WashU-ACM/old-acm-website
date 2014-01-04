3797f11929b257bde3bcf2845b8454434c886156
Shane Carr <shane.carr@wustl.edu>
2014-01-03 18:28:42 -0700

    Fixing CSRF issue with OmniAuth authentication.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
-
+  skip_before_filter :verify_authenticity_token, only: :create
 	def create
-	  auth_hash = request.env['omniauth.auth']
-	  render :text => auth_hash.inspect
+		auth_hash = request.env['omniauth.auth']
+		render :text => auth_hash.inspect
 	end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,8 @@ require 'omniauth-openid'
 require 'openid/store/filesystem'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :open_id, :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
+	provider :openid,
+		store: OpenID::Store::Filesystem.new('/tmp'),
+		name: 'google',
+		identifier: 'https://www.google.com/accounts/o8/id'
 end
