4ce1ebe5db057b63542447a6ed66db96b489203d
root <root@acm.wustl.edu>
2014-03-18 09:04:39 -0500

    Fixing some asset pipeline, style, and validation issues on production server

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,19 +1,26 @@
 class Project < ActiveRecord::Base
-	belongs_to :owner, class_name: "User"
-	has_and_belongs_to_many :enthusiasts, class_name: "User"
-	has_and_belongs_to_many :technologies
+  belongs_to :owner, class_name: "User"
+  has_and_belongs_to_many :enthusiasts, class_name: "User"
+  has_and_belongs_to_many :technologies
+  after_initialize :init
+
+  mount_uploader :image, ImageUploader
 	
-	mount_uploader :image, ImageUploader
-	
-	#simulated ENUM field in Projects table
-	validates_inclusion_of :state, :in => ["active", "inactive"]
-	validates_presence_of :state
-	def state
-	  read_attribute(:state)
-	end
-	def state= (value)
-	  write_attribute(:state, value)
-	end
-	
+  #simulated ENUM field in Projects table
+  validates_inclusion_of :state, :in => ["active", "inactive"]
+  validates_presence_of :state
+  def state
+    read_attribute(:state)
+  end
+  def state= (value)
+    write_attribute(:state, value)
+  end
+
+  private
+  def init
+    if self.state == "" || !self.state
+      self.state = "active"
+    end
+  end	
 	
 end
--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,9 @@ module AcmApp
 
     config.exceptions_app = self.routes
 
+    # Path for Bootstrap glyphcons
+    config.assets.paths << "#{Rails}/vendor/assets/fonts"
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
