bb89012cd241fe51a2ba2e8e98df04168c834192
root <root@acm.wustl.edu>
2014-03-19 01:18:10 -0500

    Fixing Google login on production server

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,7 @@ require 'openid/store/filesystem'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :openid, {
-    store: OpenID::Store::Filesystem.new('/tmp'),
+    store: OpenID::Store::Filesystem.new('./tmp'),
     name: 'google',
     identifier: 'https://www.google.com/accounts/o8/id'
   }
