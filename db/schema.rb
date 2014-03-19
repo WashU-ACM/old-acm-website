c814646c82d9317b83249b59d32798d493ea657a
root <root@acm.wustl.edu>
2014-03-19 01:19:12 -0500

    Changing project description data type from varchar to text

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140315085510) do
+ActiveRecord::Schema.define(version: 20140318214554) do
 
   create_table "competencies", force: true do |t|
     t.integer  "user_id"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 20140315085510) do
 
   create_table "projects", force: true do |t|
     t.string   "name"
-    t.string   "description"
+    t.text     "description"
     t.string   "state"
     t.integer  "owner_id"
     t.datetime "created_at"
