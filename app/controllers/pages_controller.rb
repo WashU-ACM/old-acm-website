fdb012b5473c18fc6bc9aceab61310d908d641a0
WashU ACM Root User <acm@su.wustl.edu>
2014-03-27 00:32:56 -0500

    Correcting the 404 page HTTP status code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,18 @@
 class PagesController < ApplicationController
-	#use this controller to add extra pages which don't
-	#require data from the database	
-	
-	def home
-		
-	end
-	def invalid_request
-	  
-	end
-	def calendar
-	  
-	end
+  #use this controller to add extra pages which don't
+  #require data from the database	
+  
+  def home
+    
+  end
+  def invalid_request
+    
+  end
+  def calendar
+    
+  end
+  
+  def not_found
+    render status: 404
+  end
 end
